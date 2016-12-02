b26bc4575384234f9c53fcaa4366ff0c0587bc84
shea g <shea g>
2016-12-02 09:38:11 +0000

    third commit to heroku

--- DavysPets/settings.py
+++ DavysPets/settings.py
@@ -99,8 +99,8 @@ WSGI_APPLICATION = 'DavysPets.wsgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+         #'ENGINE': 'django.db.backends.sqlite3',
+         #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 #
