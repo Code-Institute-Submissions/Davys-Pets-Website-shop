220555cfa14cf63e562e79856bc77ff3c2f18948
shea g <shea g>
2016-12-02 12:31:57 +0000

    fix commit to heroku

--- DavysPets/settings.py
+++ DavysPets/settings.py
@@ -99,13 +99,13 @@ WSGI_APPLICATION = 'DavysPets.wsgi.application'
 
 DATABASES = {
     'default': {
-         'ENGINE': 'django.db.backends.sqlite3',
-         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+         #'ENGINE': 'django.db.backends.sqlite3',
+         #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 #
-#CLEARDB_DATABASE_URL = os.environ.get("CLEARDB_DATABASE_URL")
-#DATABASES['default'] = dj_database_url.parse(CLEARDB_DATABASE_URL)  # Password validation
+CLEARDB_DATABASE_URL = os.environ.get("CLEARDB_DATABASE_URL")
+DATABASES['default'] = dj_database_url.parse(CLEARDB_DATABASE_URL)  # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
